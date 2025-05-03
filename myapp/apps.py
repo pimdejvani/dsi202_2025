42874368d28bf9f2badf6e2f43415441c29c6df6
pimdejvani <pimdejvani@gmail.com>
2025-05-03 23:54:17 +0700

    change docker

--- myapp/apps.py
+++ myapp/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ShopappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'shopapp'
+    name = 'myapp'
--- myproject/settings.py
+++ myproject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shopapp',
+    'myapp',
 ]
 
 MIDDLEWARE = [
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'shop.urls'
+ROOT_URLCONF = 'myproject.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'shop.wsgi.application'
+WSGI_APPLICATION = 'myproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
