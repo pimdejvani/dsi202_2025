76fb84bd951644e06a87ec0f6b4dbca4706e79be
pimdejvani <pimdejvani@gmail.com>
2025-05-04 15:11:18 +0700

    test header and footer

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -4,5 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    # แบบง่าย: ส่งข้อความ
-    return HttpResponse("Hello from myapp index!")
+    return render(request, 'myapp/index.html')
