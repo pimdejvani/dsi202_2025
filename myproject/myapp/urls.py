2a057da7e78e4c7e9aea076cb5178d8d0dd4554a
pimdejvani <pimdejvani@gmail.com>
2025-05-05 17:30:51 +0700

    try link all web page

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,12 +1,53 @@
-# from django.urls import path
-# from . import views
-
-app_name = 'myapp'   # optional แต่ถ้าใช้ชื่อซ้ำกับแอปอื่น จะช่วยแยกเวลา reverse()
+# myapp/urls.py
 
 from django.urls import path
-from .views import index
+from .views import (
+    HomePageView,
+    PersonalizePageView,
+    ProfilePageView,
+    PromoteStartPageView,
+    PromoteFormPageView,
+    PromoteDonePageView, 
+    HealthCampView, EngineerCampView, LanguageCampView,
+    ArchitectureCampView, VolunteerCampView, DigitalItCampView, DateCampView,
+)
+
+app_name = 'myapp'
 
 urlpatterns = [
-    # '' หมายถึง path เปล่า (root ของ include)
-    path('', index, name='index'),
+    # 1. หน้าแรก
+    path('peddlecamp/', HomePageView.as_view(), name='home'),
+
+    # แยก URL & View ของแต่ละประเภท
+    path('peddlecamp/health/', HealthCampView.as_view(), name='camp-health'),
+    path('peddlecamp/engineer/', EngineerCampView.as_view(), name='camp-engineer'),
+    path('peddlecamp/language/', LanguageCampView.as_view(), name='camp-language'),
+    path('peddlecamp/architecture/', ArchitectureCampView.as_view(), name='camp-architecture'),
+    path('peddlecamp/volunteer/', VolunteerCampView.as_view(), name='camp-volunteer'),
+    path('peddlecamp/digital_it/', DigitalItCampView.as_view(), name='camp-digital_it'),
+
+    # หน้า date (เข้าจากหน้าแรกเท่านั้น)
+    path('peddlecamp/date/', DateCampView.as_view(), name='camp-date'),
+    # 3. หน้าค้นหาตัวตน
+    path('peddlecamp/personalize_camp/', 
+         PersonalizePageView.as_view(), 
+         name='personalize-camp'),
+
+    # 4. หน้าโปรไฟล์
+    path('peddlecamp/profile/', 
+         ProfilePageView.as_view(), 
+         name='profile'),
+
+    # 5. โปรโมทกิจกรรม – เริ่มต้น
+    path('peddlecamp/promote_camp/', 
+         PromoteStartPageView.as_view(), 
+         name='promote-start'),
+    # 6. โปรโมทกิจกรรม – ฟอร์ม
+    path('peddlecamp/promote_camp/forms/', 
+         PromoteFormPageView.as_view(), 
+         name='promote-form'),
+    # 7. โปรโมทกิจกรรม – เสร็จสิ้น
+    path('peddlecamp/promote_camp/forms/done/', 
+         PromoteDonePageView.as_view(), 
+         name='promote-done'),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,7 +1,56 @@
-# myproject/myapp/views.py
+from django.views.generic import TemplateView
 
-from django.shortcuts import render
-from django.http import HttpResponse
+# 1. หน้าแรก
+class HomePageView(TemplateView):
+    template_name = 'myapp/home.html'
 
-def index(request):
-    return render(request, 'myapp/index.html')
+
+# 2. หน้าประเภทค่าย (health, engineer, …, date)
+class HealthCampView(TemplateView):
+    template_name = 'myapp/category_health.html'
+
+class EngineerCampView(TemplateView):
+    template_name = 'myapp/category_engineer.html'
+
+class LanguageCampView(TemplateView):
+    template_name = 'myapp/category_language.html'
+
+class ArchitectureCampView(TemplateView):
+    template_name = 'myapp/category_architecture.html'
+
+class VolunteerCampView(TemplateView):
+    template_name = 'myapp/category_volunteer.html'
+
+class DigitalItCampView(TemplateView):
+    template_name = 'myapp/category_digital_it.html'
+
+class OtherCampView(TemplateView):
+    template_name = 'myapp/category_other.html'
+
+class DateCampView(TemplateView):
+    template_name = 'myapp/category_date.html'
+
+
+# 3. หน้าค้นหาตัวตน
+class PersonalizePageView(TemplateView):
+    template_name = 'myapp/personalize.html'
+
+
+# 4. หน้าโปรไฟล์
+class ProfilePageView(TemplateView):
+    template_name = 'myapp/profile.html'
+
+
+# 5. โปรโมทกิจกรรม - เริ่มต้น
+class PromoteStartPageView(TemplateView):
+    template_name = 'myapp/promote_start.html'
+
+
+# 6. โปรโมทกิจกรรม - ฟอร์ม
+class PromoteFormPageView(TemplateView):
+    template_name = 'myapp/promote_form.html'
+
+
+# 7. โปรโมทกิจกรรม - เสร็จสิ้น
+class PromoteDonePageView(TemplateView):
+    template_name = 'myapp/promote_done.html'
